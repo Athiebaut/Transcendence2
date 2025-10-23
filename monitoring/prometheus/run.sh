6cea2242283b6a2de3b3d412a91ed2f2fd421504
benigne <benignedemetz@benigne.dev>
2025-10-23 19:14:03 +0200

    resetup node_exporter necessary for prometheus

---
+++ monitoring/prometheus/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#node_exporter installation
+apk update && apk upgrade && apk add curl
+
+curl -L -O https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+tar -zxvf node_exporter-1.9.1.linux-amd64.tar.gz
+mv node_exporter-1.9.1.linux-amd64/node_exporter /node_exporter
+
+chmod +x run.sh
+
+/node_exporter &
+
+./prometheus --config.file=prometheus.yml
