70aee4cb1c072f471c207cf34a5eb6f9c683029e
benigne <benignedemetz@benigne.dev>
2025-10-23 21:55:16 +0200

    end of comments

--- monitoring/grafana/run.sh
+++ monitoring/grafana/run.sh
@@ -7,6 +7,6 @@ curl -L -O https://github.com/prometheus/node_exporter/releases/download/v1.9.1/
 tar -zxvf node_exporter-1.9.1.linux-amd64.tar.gz
 mv node_exporter-1.9.1.linux-amd64/node_exporter /node_exporter
 
-/node_exporter &
+/node_exporter & #run in background to run grafana afterwards
 
 /grafana/bin/grafana server --homepath /grafana --config /grafana/grafana.ini
